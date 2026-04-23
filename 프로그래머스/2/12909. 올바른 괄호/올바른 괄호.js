786ea8061bddd6b28f1c967d4ac4d994d0b44d00
heesae <134567479+h2s0@users.noreply.github.com>
2026-04-23 11:20:52 +0900

    [level 2] Title: 올바른 괄호, Time: 27.63 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,21 +1,10 @@
 function solution(s){
     // false 인 경우 : 첫 문자가 ) 일 때, (와 )의 개수가 안맞을 때, 
-    // if (s.startsWith(")") || s.endsWith("(")) {
-    //     return false;
-    // } else if ([...s].filter(st => st === "(").length === [...s].filter(st => st === ")").length) {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
-    let stack = 0;
-    for ( let i = 0; i < s.length; i++ ) {
-        if (s[i] === "(") {
-            stack+=1
-        } else {
-            stack-=1
-            if (stack < 0) return false;
-        }
+    if (s.startsWith(")") || s.endsWith("(")) {
+        return false;
+    } else if ([...s].filter(st => st === "(").length === [...s].filter(st => st === ")").length) {
+        return true;
+    } else {
+        return false;
     }
-    return stack === 0;
-    
 }
