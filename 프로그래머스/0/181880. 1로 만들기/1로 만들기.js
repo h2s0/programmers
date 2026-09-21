21e35c256cc8e44eb20c860ee54c69ed9d344e9f
heesae <134567479+h2s0@users.noreply.github.com>
2026-09-21 15:41:37 +0900

    [level 0] Title: 1로 만들기, Time: 0.11 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let count = 0;
+    for (let i=0; i < num_list.length; i++) {
+        let num = num_list[i];
+        while(num !== 1) {
+            num = num % 2 === 0 ? num / 2 : (num -1)/2;
+            count++;
+        }
+    }
+    return count;
+}
